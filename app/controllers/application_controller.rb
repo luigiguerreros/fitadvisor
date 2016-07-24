ea8cad89ee9d55cf4bae6b7c5527660dbe0cbc4c
Luigi Guerreros <luigi.guerreros@gmail.com>
2016-07-24 20:43:15 +0000

    update devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:fullname, :gender, :country) }
-  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:fullname, :gender, :country) }
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'dashboard/index'
   get 'welcome/index'
   
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   	devise_scope :user do
   		authenticated :user do
