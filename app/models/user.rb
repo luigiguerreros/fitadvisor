4606647c0e42d8056970cf1bbeba09998a742a55
Luigi Guerreros <luigi.guerreros@gmail.com>
2016-07-31 14:11:45 +0000

    modelos

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,4 @@ class User < ApplicationRecord
 	has_many :fitpartners, :through => :friendships
 	has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
 	has_many :inverse_friends, :through => :inverse_friendships, :source => :user
-	
-	has_many :fitpartners
 end
