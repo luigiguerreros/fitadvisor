a7e0ab7cef0669fd6d6db367ce86353b89f364be
Luigi Guerreros <luigi.guerreros@gmail.com>
2016-08-03 07:05:15 +0000

    su

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ApplicationMailer
-	default from: “Web Luigi guerreros <luigi.guerreros@gmail.com>”
+	default from: "Web Luigi guerreros <luigi.guerreros@gmail.com>"
 
 	def contact_send(params) @parameters=params mail(to:'luigi.guerreros@gmail.com',subject:@parameters[:subject]) end
 end
